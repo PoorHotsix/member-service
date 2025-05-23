83e8fec6c18790ad8d5efaeab780e408ea875484
dianaseong <dianaseong31@gmail.com>
2025-05-23 17:56:47 +0900

    first commit

---
+++ src/main/java/com/example/member_service/domain/Role.java
@@ -0,0 +1,5 @@
+package com.example.member_service.domain;
+
+public enum Role {
+    USER, ADMIN
+}
---
+++ src/main/java/com/example/member_service/domain/Status.java
@@ -0,0 +1,5 @@
+package com.example.member_service.domain;
+
+public enum Status {
+    ACTIVE, WITHDRAW
+}
